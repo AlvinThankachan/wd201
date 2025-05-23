094ff5f80f5d37caf177cb219dd3ff6ce3b3b4ff
Alvin Thankachan <127949649+AlvinThankachan@users.noreply.github.com>
2025-05-23 12:01:39 +0530

    Add files via upload

---
+++ http-server/index.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+
+// Parse command line arguments
+const args = minimist(process.argv.slice(2));
+
+// Get port from command line argument, default to 3000 if not provided
+const port = args.port || 3000;
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
